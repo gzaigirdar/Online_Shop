b688a42d8b213e9cd5816e85410be316baf57d6b
gzaigirdar <90423708+gzaigirdar@users.noreply.github.com>
2026-04-16 23:13:49 -0400

    controllers from Inquiry model has been added

---
+++ Backend/src/Controllers/Inquiry_controllers/Inquiry_controllers.js
@@ -0,0 +1,51 @@
+import AsyncHandler from "express-async-handler";
+import InquiryModel from "../../Models/Inquiry/InquiryModel";
+import userModel from "../../Models/userModel";
+
+
+
+const GetInquiries = AsyncHandler(async (req,res,next) => {
+
+    const inquiries = await InquiryModel.find().sort({createdAt:-1})
+
+    if(inquiries.length === 0){
+        res.status(404)
+        throw new Error('No inquires found')
+    }
+    
+    res.send(inquiries);
+
+
+})
+
+
+const submitInquiry = AsyncHandler(async (req,res,next) => {
+
+        const {id,message} = req.body;
+
+        if (!id || !message){
+            res.status(400)
+            throw new Error('Message or id not included')
+        }
+        const submitted = await InquiryModel.create({UserId:id,
+                                                message:message})
+        res.status(200).send('submitted')
+
+        })
+
+const deleteInquiry = AsyncHandler(async (req,res,next)=> {
+    const {id} = req.body;
+
+    if(!id){
+        res.status(400)
+        throw new Error('Id not submitted')
+    }
+
+    const deleted = await InquiryModel.deleteOne({'UserId':id})
+    if (deleted.deletedCount === 0) {
+        return res.status(404).send('Inquiry not found');
+    }
+    res.status(200)
+    res.send('Inquiry deleted')
+
+})
