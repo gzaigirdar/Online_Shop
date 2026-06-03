049ea3f9401adbd4a67f5d64aad643d6683a2db4
gzaigirdar <90423708+gzaigirdar@users.noreply.github.com>
2026-06-03 00:29:18 -0400

    delete confirmation step added to delete product feature

--- Frontend/e_shop/src/components/DashBoard/Inventory/AddProd.jsx
+++ Frontend/e_shop/src/components/DashBoard/Inventory/AddProd.jsx
@@ -1,4 +1,4 @@
-import {useForm} from 'react-hook-form'
+ import {useForm} from 'react-hook-form'
 import {z} from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Inventory_context } from '@/components/context/products_context';
--- Frontend/e_shop/src/components/DashBoard/Inventory/InvRow.jsx
+++ Frontend/e_shop/src/components/DashBoard/Inventory/InvRow.jsx
@@ -7,6 +7,9 @@ function InvRow({id,name,cake_type,price,quantity}) {
 
     const {delete_product} = useContext(Inventory_context);
     const [showEdit, setShowEdit] = useState(false);
+    const[confdelete,setDelete] = useState(false);
+
+    const hideDelete = () => setDelete(false);
  
     const data = {
       "_id": id,
@@ -38,13 +41,40 @@ function InvRow({id,name,cake_type,price,quantity}) {
                 <td className="py-3 px-4">{quantity}</td>
                 <td className="py-3 px-4 space-x-2">
                   <button onClick={() => setShowEdit(true)} className="px-3 py-1 bg-yellow-500 hover:bg-yellow-600 text-white text-xs rounded">Edit</button>
-                  <button onClick={del_item} className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white text-xs rounded">Delete</button>
+                  <button onClick={()=> setDelete(true)} className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white text-xs rounded">Delete</button>
                 </td>
               </tr>
 
               <DashModal open={showEdit} setOpen={setShowEdit}>
                 <Edit_item closeit={() => setShowEdit(false)} details={data} />
               </DashModal>
+              <div>
+                <DashModal open={confdelete} setOpen={hideDelete} >
+                                      <>
+                  <div>
+                  
+          
+                  <span className=" items-center rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-gray-700 text-center">
+                      {name}
+                      </span>
+                  </div>
+
+                  <div className="mt-4 rounded-lg bg-red-50 p-3">
+                      <p className="text-xs text-red-600">
+                      This will delete the Product Permanently.
+                      </p>
+                  </div>
+                  <div>
+                  <button
+                      onClick={del_item} className="mt-4 w-full rounded-lg bg-red-600 py-2 text-sm font-medium text-white transition hover:bg-red-700"
+                      >
+                      Delete Product
+                  </button>
+                
+                  </div>
+              </>
+                </DashModal>
+              </div>
         
       </>
      );
